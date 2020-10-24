59503f650a77e9713aae38df9a9e7fda36f29eeb
abhaykatyal98 <abhaykatyal1998@gmail.com>
2020-10-24 01:13:47 -0500

    Implemented PointDifference feature

--- featureExtraction.py
+++ featureExtraction.py
@@ -8,3 +8,31 @@ class ExampleFeature(Feature) :
 	# player_color is one of chess.WHITE or chess.BLACK
 	def extract(self, game, player_color) :
 		return 50
+
+class PointDifference(Feature) :
+	
+	piece_values = {chess.PAWN: 1, chess.KNIGHT: 3, chess.BISHOP: 3, chess.ROOK: 5, chess.QUEEN: 9}
+
+	# game is a chess board from the chess library
+	# player_color is one of chess.WHITE or chess.BLACK
+	def extract(self, game, player_color) :
+		pieces = []
+		for sq in chess.SQUARES :
+			p = game.piece_at(sq)
+			if p != None and p.piece_type != chess.KING:
+				pieces.append(p)
+
+		total = 0
+		for p in pieces :
+			value = self.piece_values[p]
+			if player_color != p.color :
+				value *= -1
+			total += value
+		
+		return total
+
+		
+
+
+
+		
